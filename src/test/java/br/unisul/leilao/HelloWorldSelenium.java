424d4db7196dba68c836c09712809c1ad5c48cac
jwernner <jorge.werner@gmail.com>
2023-06-06 23:31:39 -0300

    comentários Olá Mundo

--- src/test/java/br/unisul/leilao/HelloWorldSelenium.java
+++ src/test/java/br/unisul/leilao/HelloWorldSelenium.java
@@ -8,10 +8,20 @@ public class HelloWorldSelenium {
 	
 	@Test
 	public void hello() {
+		
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		
 		WebDriver browser = new ChromeDriver();
+		// abrir o navegador classe principal do Selenium WebDriver, criando uma varíavel browser do tipo webdriver
+		// sendo uma interface, no momento da instanciação precisamos passar qual será a implementação. 
+		// já que queremos utilizar o Chrome, instanciaremos um novo ChromeDriver()
+			
 			browser.navigate().to("http://localhost:8090/leiloes");
+			// abrirmos o endereço da aplicação. 
+			// a partir da variável browser, chamaremos o método navigate().to() passando uma string com o endereço da página que queremos acessar. 
+			
 			browser.quit();
+			// para não mantermos uma janela do navegador aberta, vamos fechá-la como método browser.quit().
 	}
 
 }
